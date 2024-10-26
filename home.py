cdffcfdf702b5c23f7689bdd60193f7f440e7ee8
SpyerNot <low_li_wen@students.edu.sg>
2024-10-26 20:50:11 +0800

    Update home.py

--- home.py
+++ home.py
@@ -9,7 +9,7 @@ elif page == "Achievements":
   st.subheader("Welcome to the achievements page")
   col1, col2, col3 = st.columns(3)
   with col1:
-    st.image("1727158029439-6a41bfa3-70ed-4cec-a352-9aade17fc658_1")
+    st.image("C:\Users\lowki\OneDrive\Pictures\Capture.PNG")
 elif page == "Experiences":
   st.subheader("Welcome to the experiences page")
 else:
