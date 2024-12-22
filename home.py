cab968df7a5f7c5c03d9ecc32fb9332bc82fc134
SpyerNot <lowkimsenglowkimseng8@gmail.com>
2024-12-22 17:38:35 +0800

    Update home.py

--- home.py
+++ home.py
@@ -1,2 +1,4 @@
 import streamlit as st
 st.set_page_config(page_title="Home page")
+st.title("Low Li Wen's e-portfolio")
+st.write("Welcome to my portfolio, lets take a look at the different option")
