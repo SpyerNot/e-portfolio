bd82d9cad54e18e9db34d3e3ea1850fc035a98ee
SpyerNot <lowkimsenglowkimseng8@gmail.com>
2024-12-22 09:53:55 +0000

    updated commit

--- home.py
+++ home.py
@@ -2,3 +2,7 @@ import streamlit as st
 st.set_page_config(page_title="Home page")
 st.title("Low Li Wen's e-portfolio")
 st.write("Welcome to my portfolio, lets take a look at the different option")
+st.page_link("home.py",label="home",icon="🏠")
+st.page_link("about me.py",label="About me :D",icon="😎")
+st.page_link("projects.py",label="My projects",icon="💻")
+st.page_link("achievements.py",label="My achievements",icon="🏆")
---
+++ pages/achievements.py
@@ -0,0 +1,2 @@
+st.title("My achievements")
+st.image("cert/es2022.jpg",caption="Edusave Scholarship Sec 1",width=400)
