553e5ceec6570e7a846e8bc9a49476685b801f62
SpyerNot <lowkimsenglowkimseng8@gmail.com>
2024-12-22 10:14:59 +0000

    update

--- pages/achievements.py
+++ pages/achievements.py
@@ -2,11 +2,11 @@ import streamlit as st
 st.title("My achievements")
 col1, col2, col3=st.columns(3)
 with col1:
-    st.image("cert/es2022")
+    st.image("cert/es2022.jpg")
     st.caption=("Edusave Scholarship Sec 1")
 with col2:
-    st.image("cert/emb22023")
+    st.image("cert/emb22023.jpg")
     st.caption=("Edusave Merit Bursary Sec 2")
 with col3:
-    st.image("cert/alpchange2023")
+    st.image("cert/alpchange2023.jpg")
     st.caption("Applied Learning Programme Changemaker award Sec 2")
