e100ba1546d660570c8c5fc7320ff0be6e018503
SpyerNot <lowkimsenglowkimseng8@gmail.com>
2024-12-23 04:09:40 +0800

    update

--- pages/projects.py
+++ pages/projects.py
@@ -1,5 +1,14 @@
 import streamlit as st
 st.title("My projects")
-popover = st.popover("Open me up!!",label="Click to view my project")
+st.markdown("Open the popover")
+popover = st.popover("Open me up!!")
+tackling_obesity = popover.radio("Tacking Obesity project")
+my_portfolio = popover.radio("My e-portfolio project")
+if tackling_obesity:
+    st.link_button("Fitness app", "https://designinnovationapp-4wh5oserqjkuznsnik2pwq.streamlit.app")
+    st.link_button("Github repo","https://github.com/SpyerNot/design_innovation_app")
+else:
+    st.write("I wrote this e-portfolio from scratch using python so no app link is provided since your already here :D")
+    st.link_button("Github repo","https://e-portfolio-8wugkwxplflpwfpwjzgirj.streamlit.app")
 
 
