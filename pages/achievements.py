f83402535d444e8c3bc06a56f92886bbd724a70f
SpyerNot <lowkimsenglowkimseng8@gmail.com>
2024-12-23 03:22:37 +0800

    updated

--- pages/achievements.py
+++ pages/achievements.py
@@ -2,15 +2,32 @@ import streamlit as st
 st.title("My achievements")
 col1, col2, col3=st.columns(3)
 with col1:
-    st.image("cert/es2022.jpg",caption="Edusave Scholarship Sec 1")
+    st.image("cert/es2022.jpg",caption="Edusave Scholarship 2022 (Sec 1)")
 with col2:
-    st.image("cert/emb12023.jpg",caption="Edusave Certificate of Academic Achievement Sec 2")
+    st.image("cert/emb12023.jpg",caption="Edusave Certificate of Academic Achievement 2023 (Sec 2)")
 with col3:
-    st.image("cert/emb22023.jpg",caption="Edusave Merit Bursary Sec 2")
+    st.image("cert/emb22023.jpg",caption="Edusave Merit Bursary 2023 (Sec 2)")
 col4,col5,col6=st.columns(3)
 with col4:
-    st.image("cert/eagles2023.jpg",caption="Edusave")
+    st.image("cert/eagles2023.jpg",caption="Edusave Award for Achievement, Good Leadership and Service 2023 (Sec 2)")
 with col5:
-    st.image("cert/alpchange2023.jpg",caption="Applied Learning Programme Changemaker Award Sec 2")
+    st.image("cert/alpchange2023.jpg",caption="Applied Learning Programme Changemaker Award 2023 (Sec 2)")
 with col6:
-    st.image("cert/alpstellar2024.jpg",caption="Applied Learning Programme Stellar Award Sec 3")
+    st.image("cert/alpchange2024.jpg",caption="Applied Learning Programme Changemaker Award 2024 (Sec 3)")
+col7,col8,col9=st.columns(3)
+with col7:
+    st.image("cert/alpinnov2024.jpg",caption="Applied Learning Programme Innovation Award 2024 (Sec 3)")
+with col8:
+    st.image("cert/stellar2024",caption="Applied Learning Programme Stellar Award 2024 (Sec 3)")
+with col9:
+    st.image("cert/dic2024",caption="S1 Design Innovation Challenge 2024 2nd prize (Sec 3)")
+col10,col11,col12=st.columns(3)
+with col10:
+    st.image("cert/marchconf",caption="BuildingBloCS March Conference 2024 (A conference for students to learn more about computing) (Sec3)")
+with col11:
+    st.image("cert/marchhack",caption="BuildingBloCS March Conference Hackathon 2024, First Place (Sec 3)")
+with col12:
+    st.image("cert/juneconf",caption="BuildingBloCS June Conference 2024 (Sec 3)")
+col13,col14,col15=st.columns(3)
+with col13:
+    st.image("cert/junehack",caption="BuildingBloCS June Conference Hackathon 2024 (Sec 3)")
