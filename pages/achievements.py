c193ec1238829cba7e381d765eed53fd155eabfb
SpyerNot <lowkimsenglowkimseng8@gmail.com>
2024-12-22 10:17:47 +0000

    update

--- pages/achievements.py
+++ pages/achievements.py
@@ -4,8 +4,6 @@ col1, col2, col3=st.columns(3)
 with col1:
     st.image("cert/es2022.jpg",caption="Edusave Scholarship Sec 1")
 with col2:
-    st.image("cert/emb22023.jpg")
-    st.caption("Edusave Merit Bursary Sec 2")
+    st.image("cert/emb22023.jpg",caption="Edusave Merit Bursary Sec 2")
 with col3:
-    st.image("cert/alpchange2023.jpg")
-    st.caption("Applied Learning Programme Changemaker award Sec 2")
+    st.image("cert/alpchange2023.jpg",caption="Applied Learning Programme Changemaker award Sec 2")
