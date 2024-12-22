e505a4e6b303f02693f0ada5092c54f95ae7442d
SpyerNot <lowkimsenglowkimseng8@gmail.com>
2024-12-23 03:58:58 +0800

    update

--- pages/about me.py
+++ pages/about me.py
@@ -1,4 +1,1 @@
 import streamlit as st
-col13,col14,col15=st.columns(3)
-with col13:
-    st.image("cert/junehack.png",caption="Hi")
---
+++ pages/projects.py
@@ -0,0 +1,5 @@
+import streamlit as st
+st.title("My projects")
+popover = st.popover("Open me up!!")
+st.markdown("Click to see my project")
+
