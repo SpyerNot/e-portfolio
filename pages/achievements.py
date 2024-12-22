2e89527984289fd67a210433a8aed7aee984961a
SpyerNot <lowkimsenglowkimseng8@gmail.com>
2024-12-22 10:08:07 +0000

    updated

--- pages/achievements.py
+++ pages/achievements.py
@@ -1,3 +1,3 @@
 import streamlit as st
 st.title("My achievements")
-st.image("cert/es20222.jpg",caption="Edusave Scholarship Sec 1",use_container_width=True)
+st.image("cert/es2022.jpg",caption="Edusave Scholarship Sec 1",use_container_width=True)
