4e14c590369d4b3c2e78ec43c5ef977d905d3c41
SpyerNot <lowkimsenglowkimseng8@gmail.com>
2024-12-23 04:00:44 +0800

    update

--- pages/projects.py
+++ pages/projects.py
@@ -1,5 +1,5 @@
 import streamlit as st
 st.title("My projects")
-popover = st.popover("Open me up!!")
-st.markdown("Click to see my project")
+popover = st.popover("Open me up!!",label="Click to view my project")
+
 
