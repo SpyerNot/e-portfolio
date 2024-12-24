b27cf4aecc1dbd132bc10e08683090f13f08eb20
SpyerNot <lowkimsenglowkimseng8@gmail.com>
2024-12-24 17:35:26 +0800

    update

--- pages/projects.py
+++ pages/projects.py
@@ -10,11 +10,11 @@ if tackling_obesity:
 if my_portfolio:
     st.write("I wrote this e-portfolio from scratch using python so no app link is provided since you are already here :D")
     st.link_button("Github repo","https://e-portfolio-8wugkwxplflpwfpwjzgirj.streamlit.app")
-option = st.radio("Pick a project you would like to view:",["Tackling Obesity","E-portfolio"])
+option = st.radio("Pick a project you would like to view:",["Tackling Obesity","E-portfolio"],disabled=True)
 if option == "Tackling Obesity":
     st.html("<a href='https://designinnovationapp-4wh5oserqjkuznsnik2pwq.streamlit.app/'> Fitness App </a>")
     st.html("<a href='https://github.com/SpyerNot/design_innovation_app'>Github repo</a>")
 if option == "E-portfolio":
-    st.write("***You are currently viewing thr project E-portfolio, this e-portfolio is written in python***")
+    st.write("***You are currently viewing the project E-portfolio, this e-portfolio is written in python***")
     st.html("<a href='https://e-portfolio-8wugkwxplflpwfpwjzgirj.streamlit.app'>Github repo</a>")
 
