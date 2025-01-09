9b6f26e46ae0b3a6fd018a057ef0af3e2a5143e9
SpyerNot <lowkimsenglowkimseng8@gmail.com>
2025-01-09 08:09:13 +0800

    update

--- pages/about me.py
+++ pages/about me.py
@@ -1,7 +1,6 @@
 import streamlit as st
 st.html("<style>.name{font-family: 'Lucida Handwriting', cursive;}</style>")
-st.html("<style>id{font-size: 100px;}</style>")
+st.html("<style>h1{font-size: 40px;}</style>")
 st.html("<h1>About Me!</h1>")
-st.title("About Me!")
 st.html("<p id = 'name'>My name is <span class='name'>Low Li Wen </span> from Serangoon Garden Secondary School")
 
