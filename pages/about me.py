b0af084c59fd4d73e9e3befde3c8cc0380f77742
SpyerNot <lowkimsenglowkimseng8@gmail.com>
2025-01-27 14:10:56 +0800

    Update about me.py

--- pages/about me.py
+++ pages/about me.py
@@ -1,6 +1,9 @@
 import streamlit as st
 st.html("<style>.name{font-family: 'Lucida Handwriting', cursive;}</style>")
 st.html("<style>h1{font-size: 40px;}</style>")
-st.html("<h1>About Me!</h1>")
-st.html("<p id = 'name'>My name is <span class='name'>Low Li Wen </span> from Serangoon Garden Secondary School I am from Robotics CCA that likes to code. I have learnt the coding languages: Python and Javascript. The markup language HTML and the style sheet CSS.")
+st.html("<h1><b>About Me!</b></h1>")
+st.html("<p id = 'name'>My name is <span class='name'>Low Li Wen </span> from Serangoon Garden Secondary School I am from Robotics CCA that likes to code. I have learnt the coding languages: Python and Javascript. The markup language HTML and the style sheet CSS. I am a student that likes to smile and laugh a lot in hopes to make other people smile :D ")
+st.html("<div></div>")
+st.html("<h1><b>My Passion!</b></h1>
+
 
