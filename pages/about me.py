47f018b5a9220caa76233387e5f913bbc3568c27
SpyerNot <lowkimsenglowkimseng8@gmail.com>
2025-01-28 21:02:31 +0800

    update

--- pages/about me.py
+++ pages/about me.py
@@ -5,6 +5,6 @@ st.html("<h1><b>About Me!</b></h1>")
 st.html("<p id = 'name'>My name is <span class='name'>Low Li Wen </span> from Serangoon Garden Secondary School I am from Robotics CCA that likes to code. I have learnt the coding languages: Python and Javascript. The markup language HTML and the style sheet CSS. I am a student that likes to smile and laugh a lot in hopes to make other people smile :D ")
 st.html("<div></div>")
 st.html("<h1><b>My Passion!</b></h1>")
-st.html("<p id = 'passion'>My passion started around 8 years old, I went for different workshops for microbit, scratch, airblock drone and sphero when I was in primary school. After stepping into secondary school, my CCA was robotics to further pursue my passion in coding, in CCA we did scratch, microbit and tello drone. At upper secondary, my school offered O-Level Computing 
+st.html("<p id = 'passion'>My passion started around 8 years old, I went for different workshops for microbit, scratch, airblock drone and sphero when I was in primary school. After stepping into secondary school, my CCA was robotics to further pursue my passion in coding, in CCA we did scratch, microbit and tello drone. At upper secondary, my school offered O-Level Computing</p>") 
 
 
