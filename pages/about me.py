7313f9c58732d0f0931cc9dec71406de7d59e620
SpyerNot <lowkimsenglowkimseng8@gmail.com>
2025-02-07 14:14:41 +0800

    update

--- pages/about me.py
+++ pages/about me.py
@@ -4,7 +4,7 @@ st.html("<style>h1{font-size: 40px;}</style>")
 st.html("<h1><b>About Me!</b></h1>")
 st.html("<p id = 'name'>My name is <span class='name'>Low Li Wen </span> from Serangoon Garden Secondary School I am from Robotics CCA that likes to code. I have learnt the coding languages: Python and Javascript. The markup language HTML and the style sheet CSS. I am a student that likes to smile and laugh a lot in hopes to make other people smile :D ")
 st.html("<div></div>")
-st.html("<h1 = 'right'><b>My Passion!</b></h1>")
+st.html("<h1 align = 'right'><b>My Passion!</b></h1>")
 st.html("<p id = 'passion'>My passion started around 8 years old, I went for different workshops for microbit, scratch, airblock drone and sphero when I was in primary school. After stepping into secondary school, my CCA was robotics to further pursue my passion in coding, in CCA we did scratch, microbit and tello drone. At upper secondary, my school offered O-Level Computing so I took this subject and started learning python coding. At the end of the year, I got top in computing. During my Sec 3 year, I signed up for multiple workshop. For example, BuildingBloCS March,June and December conference. I also took up a few competitions which include hackathon and CTF. </p>") 
 
 
