f15391ea0e74183615ebc8f814f64988f4a20e73
SpyerNot <lowkimsenglowkimseng8@gmail.com>
2025-02-19 17:00:50 +0800

    added a new line for it to not get deleted

--- pages/about me.py
+++ pages/about me.py
@@ -8,6 +8,7 @@ with col1:
 st.html("<div></div>")
 st.html("<h1 align = 'right'><b>My Passion!</b></h1>")
 with col3:
-    st.html("<p id = 'passion'>My passion started around 8 years old, I went for different workshops for microbit, scratch, airblock drone and sphero when I was in primary school. After stepping into secondary school, my CCA was robotics to further pursue my passion in coding, in CCA we did scratch, microbit and tello drone. At upper secondary, my school offered O-Level Computing so I took this subject and started learning python coding. At the end of the year, I got top in computing. During my Sec 3 year, I signed up for multiple workshop. For example, BuildingBloCS March,June and December conference. I also took up a few competitions which include hackathon and CTF. </p>") 
+    st.html("<p id = 'passion'>My passion started around 8 years old, I went for different workshops for microbit, scratch, airblock drone and sphero when I was in primary school. After stepping into secondary school, my CCA was robotics to further pursue my passion in coding, in CCA we did scratch, microbit and tello drone. At upper secondary, my school offered O-Level Computing so I took this subject and started learning python coding. At the end of the year, I got top in computing. During my Sec 3 year, I signed up for multiple workshop. For example, BuildingBloCS March,June and December conference. I also took up a few competitions which include hackathon and CTF. </p>")
+st.html("<p>hi</p>")
 
 
