7643e2c9d0a6b2c395e01c57c849018cfe3dddaf
SpyerNot <lowkimsenglowkimseng8@gmail.com>
2025-07-10 11:05:59 +0800

    Update projects.py

--- pages/projects.py
+++ pages/projects.py
@@ -6,6 +6,7 @@ popover = st.popover("Open me up!!")
 tackling_obesity = popover.checkbox("Tacking Obesity project")
 my_portfolio = popover.checkbox("My e-portfolio project")
 translator = popover.checkbox("Translator project")
+secret_santa = popover.checkbox("Secret Santa project")
 if tackling_obesity:
     st.link_button("Fitness app", "https://designinnovationapp-4wh5oserqjkuznsnik2pwq.streamlit.app")
     st.link_button("Github repo","https://github.com/SpyerNot/design_innovation_app")
@@ -16,7 +17,11 @@ if translator:
     st.link_button("Translator App","https://translator-ixyqcckissjuf57apt9kec.streamlit.app/")
     st.link_button("Github repo", "https://github.com/SpyerNot/Translator")
     st.write("This is one of my hackathon project with my team to solve the issue on slur speech communication")
-option = st.radio("Pick a project you would like to view:",["Tackling Obesity","E-portfolio","Translator"])
+if secret_santa:
+    st.link_button("Secret Santa website", "https://secretsanta22.vercel.app/")
+    st.link_button("Github repo","https://github.com/frosetrain/secretsanta")
+    st.write("This is a project for hackathon, I am invited as the collaborator")
+option = st.radio("Pick a project you would like to view:",["Tackling Obesity","E-portfolio","Translator","Secret Santa"])
 if option == "Tackling Obesity":
     st.link_button("Fitness app","https://designinnovationapp-4wh5oserqjkuznsnik2pwq.streamlit.app")
     st.link_button("Github Repo","https://github.com/SpyerNot/design_innovation_app")
@@ -27,6 +32,10 @@ if option == "Translator":
     st.link_button("Translator App","https://translator-ixyqcckissjuf57apt9kec.streamlit.app/")
     st.link_button("Github repo", "https://github.com/SpyerNot/Translator")
     st.write("This is one of my hackathon project with my team to solve the issue on slur speech communication")
+if option == "Secret Santa":
+    st.link_button("Secret Santa website", "https://secretsanta22.vercel.app/")
+    st.link_button("Github repo","https://github.com/frosetrain/secretsanta")
+    st.write("This is a project for hackathon, I am invited as the collaborator")
 
 
 
