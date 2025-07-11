73c4c5354137f8cdea96f84a02c4be0599ce2668
SpyerNot <lowkimsenglowkimseng8@gmail.com>
2025-07-11 19:42:40 +0800

    changed experiences.py

--- pages/experiences.py
+++ pages/experiences.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from streamlit_timeline import timeline
+import streamlit_timeline
 st.set_page_config(page_title="Test Basic Timelines", layout="wide")
 st.title("My Experiences")
 with open('timeline.json','r') as f:
