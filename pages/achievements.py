af83a143beaa6b86e8d522730011fa561a8c4d6a
SpyerNot <lowkimsenglowkimseng8@gmail.com>
2025-07-11 16:24:10 +0800

    typo in photo link

--- pages/achievements.py
+++ pages/achievements.py
@@ -60,6 +60,6 @@ with col24:
     st.image("cert/YCEP2025.png",caption="YCEP Bootcamp 2025 participation certificate at NYP (Sec 4)")
 col25,col26,col27=st.columns(3)
 with col25:
-    st.image("cert/YCEP2025indiv.png",caption="YCEP Finals 2025 Individual Category (Sec 4)")
+    st.image("cert/YCEP2025indvi.png",caption="YCEP Finals 2025 Individual Category (Sec 4)")
 with col26:
     st.image("cert/Low Li Wen_Certificate_of_Excellence_Group.png",caption="YCEP Finals 2025 6th Place for Group Category (Sec 4)")
