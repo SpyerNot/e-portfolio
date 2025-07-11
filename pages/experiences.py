9d120c5d6309b5e12384b3fa29a0f4583ea3d54c
SpyerNot <lowkimsenglowkimseng8@gmail.com>
2025-07-11 19:44:45 +0800

    added requirements.txt

--- pages/experiences.py
+++ pages/experiences.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import streamlit_timeline
+from streamlit_timeline import timeline
 st.set_page_config(page_title="Test Basic Timelines", layout="wide")
 st.title("My Experiences")
 with open('timeline.json','r') as f:
