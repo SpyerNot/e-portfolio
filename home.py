f162589f16d6eccf52135eab691641086ab88c11
SpyerNot <lowkimsenglowkimseng8@gmail.com>
2025-07-11 23:41:35 +0800

    added more recommendation letters

--- home.py
+++ home.py
@@ -10,8 +10,20 @@ st.divider()
 cybersec = "https://docs.google.com/document/d/1PaYX0i1-vBfCUTf_upenT20NChQDB5nj/edit?usp=sharing&ouid=104159583048794347910&rtpof=true&sd=true"
 ict="https://docs.google.com/document/d/1PenPIkocIHWqWuSZoc0WKQHKJDJXGnZ2/edit?usp=sharing&ouid=104159583048794347910&rtpof=true&sd=true"
 clw="https://docs.google.com/document/d/1P_lwAi3vWpR7oyutGNJnFxixdEStPJld/edit?usp=sharing&ouid=104159583048794347910&rtpof=true&sd=true"
-st.markdown(
-    f'<iframe src="{cybersec}&rm=minimal" width="800" height="600"></iframe>',
-    unsafe_allow_html=True,
-)
+col1,col2,col3=st.columns(3)
+with col1:
+    st.markdown(
+        f'<iframe src="{cybersec}&rm=minimal" width="800" height="600"></iframe>',
+        unsafe_allow_html=True,
+    )
+with col2:
+    st.markdown(
+        f'<iframe src="{ict}&rm=minimal" width="800" height="600"></iframe>',
+        unsafe_allow_html=True,
+    )
+with col3:
+    st.markdown(
+        f'<iframe src="{clw}&rm=minimal" width="800" height="600"></iframe>',
+        unsafe_allow_html=True,
+    )
 st.write("If you got any queries, contact me at lowliwen2409@gmail.com")
