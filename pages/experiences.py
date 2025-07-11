8ff20f519a4f5dd0148a090c9c4951efa01a5207
SpyerNot <lowkimsenglowkimseng8@gmail.com>
2025-07-11 19:33:04 +0800

    added timeline

--- pages/experiences.py
+++ pages/experiences.py
@@ -1,4 +1,7 @@
 import streamlit as st
 from streamlit_timeline import timeline
 st.title("My Experiences")
-
+st.set_page_config(page_title="Test Basic Timelines", layout="wide")
+with open('timeline.json','r') as f:
+    data = f.read()
+timeline(data,height=600)
