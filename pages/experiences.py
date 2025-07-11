79928a306b9fdd67130524ad278d5792de11770c
SpyerNot <lowkimsenglowkimseng8@gmail.com>
2025-07-11 19:38:45 +0800

    edited experience.py line 3 and 4

--- pages/experiences.py
+++ pages/experiences.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from streamlit_timeline import timeline
-st.title("My Experiences")
 st.set_page_config(page_title="Test Basic Timelines", layout="wide")
+st.title("My Experiences")
 with open('timeline.json','r') as f:
     data = f.read()
 timeline(data,height=600)
