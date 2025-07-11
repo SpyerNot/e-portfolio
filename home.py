ae731fe005595b50a940caefeba1bd7734549aae
SpyerNot <lowkimsenglowkimseng8@gmail.com>
2025-07-11 23:38:12 +0800

    added docs

--- home.py
+++ home.py
@@ -7,4 +7,11 @@ st.page_link("pages/about me.py",label="About me :D",icon="😎")
 st.page_link("pages/projects.py",label="My projects",icon="💼")
 st.page_link("pages/achievements.py",label="Achievements",icon="🏆")
 st.divider()
+cybersec = "https://docs.google.com/document/d/1PaYX0i1-vBfCUTf_upenT20NChQDB5nj/edit?usp=sharing&ouid=104159583048794347910&rtpof=true&sd=true"
+ict="https://docs.google.com/document/d/1PenPIkocIHWqWuSZoc0WKQHKJDJXGnZ2/edit?usp=sharing&ouid=104159583048794347910&rtpof=true&sd=true"
+clw="https://docs.google.com/document/d/1P_lwAi3vWpR7oyutGNJnFxixdEStPJld/edit?usp=sharing&ouid=104159583048794347910&rtpof=true&sd=true"
+st.markdown(
+    f'<iframe src="{cybersec}&rm=minimal" width="800" height="600"></iframe>',
+    unsafe_allow_html=True,
+)
 st.write("If you got any queries, contact me at lowliwen2409@gmail.com")
