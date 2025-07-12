e4799216293cf38d84d96f945c832c9134e0866e
SpyerNot <lowkimsenglowkimseng8@gmail.com>
2025-07-13 01:41:09 +0800

    edited captions

--- pages/about me.py
+++ pages/about me.py
@@ -7,7 +7,7 @@ col1,col2,col3=st.columns(3)
 with col1:
     st.image("pages/Screenshot_20250711_231947_WhatsApp.jpg", caption="Me when I was bowling")
 with col2:
-    st.image("pages/cube.jpg",caption="Me in my first ever speed cubing competition")
+    st.image("pages/cube.jpg",caption="Me practicing and got a 11s :D")
 st.html("<div></div>")
 st.html("<h1 align = 'right'><b>My Passion!</b></h1>")
 st.html("<p id = 'passion', align = 'right'>My passion started around 8 years old, I went for different workshops for microbit, scratch, airblock drone and sphero when I was in primary school. After stepping into secondary school, my CCA was robotics to further pursue my passion in coding, in CCA we did scratch, microbit and tello drone. At upper secondary, my school offered O-Level Computing so I took this subject and started learning python coding. At the end of the year, I got top in computing. During my Sec 3 year, I signed up for multiple workshop. For example, BuildingBloCS March, June and December conference. I also took up a few competitions which include hackathon and CTF. </p>")
